6f14021c85fb30e75eb90e334c6432dc61b6b10a
Szymon Żuk <116501563+szymon-zuk@users.noreply.github.com>
2023-11-21 11:43:25 +0000

    cosmetic changes to styling

--- app.py
+++ app.py
@@ -6,16 +6,20 @@ import random
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/draw', methods=['POST'])
+
+@app.route("/draw", methods=["POST"])
 def draw():
     generated_team = generate_random_team()
     names = [player[0] for player in generated_team]
     total_cost = sum(player[1] for player in generated_team)
-    return jsonify({'success': True, 'generated_team': generated_team, 'total_cost': total_cost})
+    return jsonify(
+        {"success": True, "generated_team": generated_team, "total_cost": total_cost}
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- scrap.py
+++ scrap.py
@@ -1,18 +1,33 @@
 import requests
 import random
 
-URL = "https://www.hltv.org/fantasy/408/leagues/160778/join/json" #JSON sent on get request when joining a specific fantasy event
+URL = "https://www.hltv.org/fantasy/408/leagues/160778/join/json"  # JSON sent on get request when joining a specific fantasy event
 response = requests.get(URL)
 
 if response.status_code == 200:
     data = response.json()
 
-    player_names = [player["playerData"]["name"] for team in data["moneyDraftData"]["teams"] for player in team["players"]]
-    players_cost = [player["cost"] for team in data["moneyDraftData"]["teams"] for player in team["players"]]
-    player_pic_links = [player["playerData"]["picture"] for team in data["moneyDraftData"]["teams"] for player in team["players"]]
-    player_stats = [player["playerData"]["stats"] for team in data["moneyDraftData"]["teams"] for player in team["players"]]
+    player_names = [
+        player["playerData"]["name"]
+        for team in data["moneyDraftData"]["teams"]
+        for player in team["players"]
+    ]
+    players_cost = [
+        player["cost"]
+        for team in data["moneyDraftData"]["teams"]
+        for player in team["players"]
+    ]
+    player_pic_links = [
+        player["playerData"]["picture"]
+        for team in data["moneyDraftData"]["teams"]
+        for player in team["players"]
+    ]
+    player_stats = [
+        player["playerData"]["stats"]
+        for team in data["moneyDraftData"]["teams"]
+        for player in team["players"]
+    ]
     player_data = list(zip(player_names, players_cost, player_pic_links, player_stats))
-
 
 
 def generate_random_team(player_data=player_data, max_cost=1000000):
@@ -21,10 +36,11 @@ def generate_random_team(player_data=player_data, max_cost=1000000):
 
     while len(generated_players) != 5:
         random_player = random.choice(available_players)
-        if sum(player[1] for player in generated_players) + random_player[1] <= max_cost:
+        if (
+            sum(player[1] for player in generated_players) + random_player[1]
+            <= max_cost
+        ):
             generated_players.append(random_player)
             available_players.remove(random_player)
 
     return generated_players
-
-print(generate_random_team())
